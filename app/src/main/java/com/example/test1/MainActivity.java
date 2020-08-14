dc68417ec1b79b21727376a92fbebec2efd3ed12
alyaladya01 <nisrinaalya0112@gmail.com>
2020-08-14 11:29:39 +0700

    add SP

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
+    EditText txtusername;
+    EditText txtpass;
+    Button  btnlogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pref = getSharedPreferences("login", MODE_PRIVATE);
+        txtusername = (EditText)findViewById(R.id.txtusername);
+        txtpass = (EditText)findViewById(R.id.txtpass);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (txtusername.getText().toString().equalsIgnoreCase("alyaladya")
+                && txtpass.getText().toString().equalsIgnoreCase("0112")){
+
+                    editor = pref.edit();
+                    editor.putString("username", txtusername.getText().toString());
+                    editor.putString("status", "login");
+                    editor.apply();
+
+                    startActivity(new Intent(getApplicationContext(), MainMenu.class));
+                    finish();
+
+
+                }
+
+            }
+        });
+
+
     }
 }
